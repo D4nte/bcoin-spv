65b7a992f03306cec806d38ac8bf74d6a2ae0135
Franck Royer <franck@coblox.tech>
2019-09-11 10:53:53 +1000

    Remove SPVNode

--- spv.js
+++ spv.js
@@ -1,78 +1,85 @@
-'use strict';
+"use strict";
 
-const bcoin = require('bcoin');
+const bcoin = require("bcoin");
 const Client = require("bitcoin-core");
-const Logger = require('blgr');
+const Logger = require("blgr");
 
 const logger = new Logger({
-  level: 'debug'
+  level: "debug"
 });
 
-bcoin.set('regtest');
+bcoin.set("regtest");
 
 const BITCOIN_AUTH = {
   protocol: "http",
   username: "bitcoin",
   password: "54pLR_f7-G6is32LP-7nbhzZSbJs_2zSATtZV_r05yg=",
   host: "localhost",
-  port: "18443",
+  port: "18443"
 };
 
 const bitcoinClient = new Client(BITCOIN_AUTH);
 
-const node = new bcoin.SPVNode({
-  network:'regtest',
-  logger,
-  memory: true
+// SPV chains only store the chain headers.
+const chain = new bcoin.Chain({
+  spv: true,
+  logger: logger
+});
+
+const pool = new bcoin.Pool({
+  chain: chain,
+  maxOutbound: 1,
+  logger: logger
 });
 
 const walletdb = new bcoin.wallet.WalletDB({ memory: true, logger: logger });
 
 (async () => {
-  await node.open();
+  await pool.open();
   await walletdb.open();
-  await node.connect();
+  await chain.open();
+  await pool.connect();
 
-  const wallet = await walletdb.create({logger: logger});
+  const wallet = await walletdb.create({ logger: logger });
   const walletAddress = await wallet.receiveAddress();
-  console.log('Created wallet with address %s', walletAddress);
+  console.log("Created wallet with address %s", walletAddress);
 
   // Add our address to the SPV filter.
-  node.pool.watchAddress(walletAddress);
+  pool.watchAddress(walletAddress);
 
   // Start the blockchain sync.
-  node.startSync();
+  pool.startSync();
 
   // Get ready to receive transactions!
-  node.on('tx', (tx) => {
-    console.log('Received TX:\n', tx);
+  pool.on("tx", tx => {
+    console.log("Received TX:\n", tx);
 
     walletdb.addTX(tx);
-    console.log('TX added to wallet DB!');
+    console.log("TX added to wallet DB!");
   });
 
-  node.on('block', async (block) => {
-    console.log('Received Block:\n', block);
+  pool.on("block", async block => {
+    console.log("Received Block:\n", block);
 
     await walletdb.addBlock(block);
-    console.log('Block added to wallet DB!');
+    console.log("Block added to wallet DB!");
     console.log("Balance:", await wallet.getBalance());
 
     if (block.txs.length > 0) {
-      block.txs.forEach((tx) => {
+      block.txs.forEach(tx => {
         walletdb.addTX(tx);
-        console.log('TX added to wallet DB!');
-      })
+        console.log("TX added to wallet DB!");
+      });
     }
   });
 
-  wallet.on('balance', (balance) => {
-    console.log('Balance updated:\n', balance.toJSON());
+  wallet.on("balance", balance => {
+    console.log("Balance updated:\n", balance.toJSON());
   });
 
-  const netAddr = await node.pool.hosts.addNode('127.0.0.1:18444');
-  const peer = node.pool.createOutbound(netAddr);
-  node.pool.peers.add(peer);
+  const netAddr = await pool.hosts.addNode("127.0.0.1:18444");
+  const peer = pool.createOutbound(netAddr);
+  pool.peers.add(peer);
 
   console.log("Peers:", await bitcoinClient.getPeerInfo());
 
@@ -83,8 +90,7 @@ const walletdb = new bcoin.wallet.WalletDB({ memory: true, logger: logger });
   await new Promise(r => setTimeout(r, 1000));
   await bitcoinClient.generate(1);
   console.log("Balance:", await wallet.getBalance());
-
-})().catch((err) => {
+})().catch(err => {
   console.error(err.stack);
   process.exit(1);
 });
