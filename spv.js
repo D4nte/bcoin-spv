2a59ea081329ba93683aca24f1354d0bf72320b8
Franck Royer <franck@royer.one>
2019-11-25 11:10:57 +1100

    Use testnet

--- spv.js
+++ spv.js
@@ -1,27 +1,18 @@
 "use strict";
 
 const bcoin = require("bcoin");
-const Client = require("bitcoin-core");
 const Logger = require("blgr");
 
 const logger = new Logger({
-  level: "debug"
+  level: "info"
 });
 
-bcoin.set("regtest");
+const NETWORK = "testnet";
 
-const BITCOIN_AUTH = {
-  protocol: "http",
-  username: "bitcoin",
-  password: "54pLR_f7-G6is32LP-7nbhzZSbJs_2zSATtZV_r05yg=",
-  host: "localhost",
-  port: "18443"
-};
-
-const bitcoinClient = new Client(BITCOIN_AUTH);
+bcoin.set(NETWORK);
 
 const node = new bcoin.SPVNode({
-  network: "regtest",
+  network: NETWORK,
   logger,
   memory: true
 });
@@ -30,19 +21,24 @@ const node = new bcoin.SPVNode({
 node.rpc = null;
 
 const walletdb = new bcoin.wallet.WalletDB({
+  network: NETWORK,
   memory: false,
-  location: "./bcoin-db/",
+  location: `./bcoin-db/${NETWORK}/`,
   logger: logger
 });
 
 (async () => {
+  await node.ensure();
   await node.open();
   await walletdb.open();
   await node.connect();
 
   const wallet = await walletdb.create({ logger: logger });
   wallet.logger = logger;
-  const walletAddress = await wallet.receiveAddress();
+  const walletAddress = new bcoin.Address(
+    "tb1qffzwg0n8rvuu88xa6rsal34f2kt7yauc964yyu",
+    "testnet"
+  );
   console.log("Created wallet with address %s", walletAddress);
 
   // Add our address to the SPV filter.
@@ -78,41 +74,12 @@ const walletdb = new bcoin.wallet.WalletDB({
     console.log("Balance updated:\n", balance.toJSON());
   });
 
-  const netAddr = await node.pool.hosts.addNode("127.0.0.1:18444");
+  const netAddr = await node.pool.hosts.addNode("35.189.30.79:18333");
   const peer = node.pool.createOutbound(netAddr);
   node.pool.peers.add(peer);
 
-  console.log("Peers:", await bitcoinClient.getPeerInfo());
-
-  await bitcoinClient.generate(101);
-  const fundingTxId = await bitcoinClient.sendToAddress(
-    walletAddress.toString(),
-    0.9
-  );
-  console.log("Transaction:", fundingTxId);
-  await bitcoinClient.generate(1);
-  const rawFundingTx = await bitcoinClient.getRawTransaction(fundingTxId);
-  console.log("rawFundingTx:", rawFundingTx);
   await new Promise(r => setTimeout(r, 1000));
-  await bitcoinClient.generate(1);
   console.log("Balance:", await wallet.getBalance());
-
-  const tx = await wallet.send({
-    witness: true,
-    outputs: [
-      {
-        address:
-          "bcrt1qp6xfd6qnun0v8ztd0jne8yve0cf2uyxaxn4mmd0akvd7ccyy49msg56d4u",
-        value: 15000
-      }
-    ]
-  });
-  await node.pool.broadcast(tx);
-  console.log("Bcoin tx hash:", tx.txid());
-  await new Promise(r => setTimeout(r, 10000));
-
-  const rawtx = await bitcoinClient.getRawTransaction(tx.txid());
-  console.log("rawtx:", rawtx);
 })().catch(err => {
   console.error(err.stack);
   process.exit(1);
